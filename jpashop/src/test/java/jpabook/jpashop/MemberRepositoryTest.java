995eaade856c569dad668c635cf2eb36308bf1aa
SeolA-Lee <dlcodus1201@naver.com>
2025-03-02 12:48:13 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 프로젝트 환경설정

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class) // JUnit5를 사용하기 때문 (JUnit4에서는 @RunWith 사용)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+}
