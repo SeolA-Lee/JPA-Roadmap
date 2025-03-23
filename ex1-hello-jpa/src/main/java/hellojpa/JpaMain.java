7f75dc2b474f2d04df847955a124b0c5e1265921
SeolA-Lee <dlcodus1201@naver.com>
2025-03-23 23:04:05 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 엔티티 매핑(14-16강)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,68 +20,22 @@ public class JpaMain {
 
         try {
 
-            /* 비영속 상태 */
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
+            Member member = new Member();
+//            member.setId(1L);
+//            member.setUsername("A");
+//            member.setRoleType(RoleType.USER);
 
-            /* 영속 상태 */
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member); // 이때 DB에 저장되는 것은 아님 -> 1차 캐시에 저장되는 것임
-//            System.out.println("=== AFTER ===");
+//            member.setId(2L);
+//            member.setUsername("B");
+//            member.setRoleType(RoleType.ADMIN);
 
-//            Member findMember = em.find(Member.class, 100L);
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
+            em.persist(member);
 
-            /**
-             * 영속성 컨텍스트 이점
-             * 1. 1차 캐시에서 조회
-             * 2. 동일성 보장
-             */
-//            Member findMember1 = em.find(Member.class, 101L); // 처음 조회할 때 영속성 컨텍스트에 올려 놓음 -> 쿼리 O
-//            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에서 조회하기 때문에 쿼리 X
-
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-            /**
-             * 3. 트랜잭션을 지원하는 쓰기 지연
-             */
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-
-//            em.persist(member1);
-//            em.persist(member2);
-
-            /**
-             * 4. 변경 감지
-             */
-//            Member member = em.find(Member.class, 150L);
-//            member.setName("ZZZZZ");
-
-            /**
-             * flush - 직접 호출
-             */
-//            Member member = new Member(200L, "member200");
-//            em.persist(member);
-
-//            em.flush(); // DB에 Insert 쿼리가 즉시 날라감
-
-            /**
-             * 준영속 상태
-             */
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-//            em.detach(member); // 영속성 컨텍스트에서 더 이상 관리하지 않게 됨 -> tx.commit() 해도 아무 일도 일어나지 않음
-
-            em.clear(); // em에 있는 영속성 컨텍스트를 통으로 다 지워버림 (초기화)
-            Member member2 = em.find(Member.class, 150L); // 다시 영속성 컨텍스트에 올리게 됨
-
-            System.out.println("===========================");
-
-            tx.commit(); // 이 시점에 쿼리가 날라감
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,21 +1,42 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+//@Table(name = "MBR")
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) // 주의! ORDINAL 사용 X
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate; // date
+    private LocalDateTime testLocalDateTime; // timestamp
+
+    @Lob
+    private String description;
+
+    @Transient // 필드 매핑 X
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -26,11 +47,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
