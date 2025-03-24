fdb3591180c3d3eee614d41bbcd1550d754f702a
SeolA-Lee <dlcodus1201@naver.com>
2025-03-24 19:16:06 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 실전 예제 - 연관관계 매핑 시작

--- jpashop-basic/src/main/java/jpabook/JpaMain.java
+++ jpashop-basic/src/main/java/jpabook/JpaMain.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,6 +18,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /**
+             * 양방향 매핑 시
+             */
+            Order order = new Order();
+            order.addOrderItem(new OrderItem()); // 연관관계 편의 메소드 사용
+
+            /**
+             * 단방향으로만 매핑 시
+             */
+//            Order order = new Order();
+//            em.persist(order);
+
+//            OrderItem orderItem = new OrderItem();
+//            orderItem.setOrder(order);
+
+//            em.persist(orderItem);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpashop-basic/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop-basic/src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -12,6 +15,9 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>(); // 실무에선 설계할 때 이 양방향이 꼭 필요한지를 생각해야 함. 여기선 예제이므로 사용
 
     public Long getId() {
         return id;
