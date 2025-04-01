b7b165b5d860e5241a16f4c8aff3eeaf6c70fc7c
SeolA-Lee <dlcodus1201@naver.com>
2025-04-01 13:13:54 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 값 타입(39강)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,9 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,30 +20,49 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city", "street", "10000");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+//            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+//            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+
             em.persist(member);
 
-            // 복사하지 않으면 Embedded 되어 있기 때문에 member1, member2 둘 다 바뀌게 됨
-//            Address copyAddress = address; // 주의! 객체 타입은 참조를 전달하기 때문에 이렇게 copy 하면 안 됨
-//            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+            em.flush();
+            em.clear();
 
-//            Member member2 = new Member();
-//            member2.setUsername("member2");
-//            member2.setHomeAddress(copyAddress);
-//            em.persist(member2);
+            System.out.println("================ START ================");
+            Member findMember = em.find(Member.class, member.getId());
 
-//            member.getHomeAddress().setCity("newCity"); // setter가 private로 설정되면 변경 불가해짐
+            /* 조회 */
+//            List<Address> addressHistory = findMember.getAddressHistory();
+//            for (Address address : addressHistory) {
+//                System.out.println("address = " + address.getCity());
+//            }
 
-            /**
-             * setter가 private 일 때 값 변경 방법
-             * : 완전히 새로 갈아끼워야 함
-             */
-            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+//            for (String favoriteFood : favoriteFoods) {
+//                System.out.println("favoriteFood = " + favoriteFood);
+//            }
+
+            /* 수정 */
+//            Address a = findMember.getHomeAddress();
+//            findMember.setHomeAddress(new Address("newcity", a.getStreet(), a.getZipcode()));
+
+            // 치킨 -> 한식
+//            findMember.getFavoriteFoods().remove("치킨");
+//            findMember.getFavoriteFoods().add("한식");
+
+            // 과거주소 변경
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
