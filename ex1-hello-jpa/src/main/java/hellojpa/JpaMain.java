5b721d666a3933a2db32b75df5eace0c2af328c3
SeolA-Lee <dlcodus1201@naver.com>
2025-04-01 11:31:00 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 값 타입(37-38강)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,12 +17,30 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            // 복사하지 않으면 Embedded 되어 있기 때문에 member1, member2 둘 다 바뀌게 됨
+//            Address copyAddress = address; // 주의! 객체 타입은 참조를 전달하기 때문에 이렇게 copy 하면 안 됨
+//            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+//            Member member2 = new Member();
+//            member2.setUsername("member2");
+//            member2.setHomeAddress(copyAddress);
+//            em.persist(member2);
+
+//            member.getHomeAddress().setCity("newCity"); // setter가 private로 설정되면 변경 불가해짐
+
+            /**
+             * setter가 private 일 때 값 변경 방법
+             * : 완전히 새로 갈아끼워야 함
+             */
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -4,12 +4,23 @@ public class ValueMain {
 
     public static void main(String[] args) {
 
+        /**
+         * 동일성(identity) 비교: 인스턴스의 참조 값을 비교, == 사용
+         */
         int a = 10;
-        int b = a;
+        int b = 10;
 
-        b = 20;
+        System.out.println("a == b: " + (a == b)); // true
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+
+        System.out.println("address1 == address2: " + (address1 == address2)); // false -> 참조값이 다르기 때문
+
+        /**
+         * 동등성(equivalence) 비교: 인스턴스의 값을 비교, equals() 사용
+         * 기본 equals()는 '==' 비교이기 때문에, Address 클래스에서 재정의 해야 함
+         */
+        System.out.println("address1 equals address2: " + (address1.equals(address2)));
     }
 }
