d274bcf59d55c81d16f25da6a8966bca96a6313e
SeolA-Lee <dlcodus1201@naver.com>
2025-04-01 09:56:10 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 프록시와 연관관계 관리(33강)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,8 +2,6 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -19,51 +17,27 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
+            em.persist(parent); // cascade 로 인해 자식들도 같이 저장됨
+//            em.persist(child1);
+//            em.persist(child2);
 
             em.flush();
             em.clear();
 
-//            Member m = em.find(Member.class, member1.getId());
-
-//            System.out.println("m = " + m.getTeam().getClass()); // 지연로딩 시 Proxy로 가져 옴
-
-//            System.out.println("===============");
-//            System.out.println("teamName = " + m.getTeam().getName()); // 지연로딩 시 초기화
-//            System.out.println("===============");
-
             /**
-             * 즉시로딩 사용 시 JPQL에서 N+1 문제를 일으킴
-             * SQL: select * from Member; -- EAGER이므로 Team 테이블도 불러옴
-             * SQL: select * from Team where TEAM_ID = xxx;
+             * 고아 객체 예제
              */
-//            List<Member> members = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
-            /**
-             * 기본 해결방안
-             * (1) LAZY로 다 바꿈
-             * (2) fetch 조인 사용
-             */
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
-                    .getResultList();
+//            em.remove(findParent);
 
             tx.commit();
         } catch (Exception e) {
