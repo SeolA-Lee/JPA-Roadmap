1307d656fd8f71b689703b3ed80abdb4cd7caa8f
SeolA-Lee <dlcodus1201@naver.com>
2025-04-02 22:14:11 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 객체지향 쿼리언어1(41강)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 import java.util.Set;
@@ -20,49 +23,55 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            /**
+             * JPQL 특징
+             * 1) 테이블이 아닌 엔티티를 대상으로 검색함
+             * 2) String이기 때문에 동적 쿼리를 해결하기 힘듦
+             */
+//            String qlString = "select m from Member m where m.username like '%kim%'";
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
+//            List<Member> result = em.createQuery(
+//                    qlString,
+//                    Member.class
+//            ).getResultList();
 
-//            member.getAddressHistory().add(new Address("old1", "street", "10000"));
-//            member.getAddressHistory().add(new Address("old2", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("================ START ================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            /* 조회 */
-//            List<Address> addressHistory = findMember.getAddressHistory();
-//            for (Address address : addressHistory) {
-//                System.out.println("address = " + address.getCity());
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
 //            }
 
-//            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-//            for (String favoriteFood : favoriteFoods) {
-//                System.out.println("favoriteFood = " + favoriteFood);
-//            }
+            /**
+             * Criteria 특징
+             * 1) 문자가 아닌 자바 코드로 JPQL 작성 가능
+             * 2) 동적 쿼리 조금 더 해결 가능
+             * 하지만! 너무 복잡하고(SQL스럽지 않음) 실용성이 없기 때문에 QueryDSL 사용을 권장함
+             */
+            /* 사용 준비 */
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            /* 수정 */
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newcity", a.getStreet(), a.getZipcode()));
+            /* 루트 클래스 (조회를 시작할 클래스) */
+//            Root<Member> m = query.from(Member.class);
 
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
+            /* 쿼리 생성 */
+//            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+//            List<Member> resultList = em.createQuery(cq).getResultList();
 
-            // 과거주소 변경
-//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
+            /**
+             * QueryDSL 특징
+             * 1) 문자가 아닌 자바 코드로 JPQL 작성 가능
+             * 2) 동적 쿼리 작성이 편리함
+             * 3) 단순하고 쉬움
+             * 따라서! 실무 사용 권장
+             */
+            // 간단한 예제: 섹션 11 - 41강 20분쯤 참고
+
+            /**
+             * 네이티브 SQL 특징
+             * 1) JPA가 제공하는 SQL을 직접 사용 가능
+             */
+//            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+//                            .getResultList();
+
 
             tx.commit();
         } catch (Exception e) {
