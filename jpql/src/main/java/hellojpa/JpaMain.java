8b82fe6fa2cd4cc503322af54c78e2b8e620b2d5
SeolA-Lee <dlcodus1201@naver.com>
2025-04-03 10:12:41 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 객체지향 쿼리언어1 - 페이징

--- jpql/src/main/java/hellojpa/JpaMain.java
+++ jpql/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,6 @@
 package hellojpa;
 
-import hellojpa.jpql.Address;
 import hellojpa.jpql.Member;
-import hellojpa.jpql.MemberDTO;
-import hellojpa.jpql.Team;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -19,54 +16,25 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            /**
-             * 엔티티 프로젝션
-             * SELECT m FROM Member m
-             */
-//            List<Member> result = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
 
-//            Member findMember = result.get(0);
-//            findMember.setAge(20);
-
-            /**
-             * 엔티티 프로젝션
-             * SELECT m.team FROM Member m
-             * -> SELECT t FROM Member m join m.team 처럼 명시적으로 join을 표현하는 것이 좋음
-             */
-//            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
-//                    .getResultList();
-
-            /**
-             * 임베디드 타입 프로젝션
-             * SELECT o.address FROM Order o
-             * 주의! SELECT a FROM Address a 처럼 실행할 수는 없음 (소속되어 있기 때문)
-             */
-//            em.createQuery("select o.address from Order o", Address.class)
-//                    .getResultList();
-
-            /**
-             * 스칼라 타입 프로젝션
-             * SELECT m.username, m.age FROM Member m
-             */
-//            em.createQuery("select distinct m.username, m.age from Member m")
-//                    .getResultList();
-
-            /* 여러 값 조회 방법 - new 명령어로 조회 */
-            List<MemberDTO> result = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();                               // 주의! 패키지명을 포함한 전체 클래스명 입력
-
-            MemberDTO memberDTO = result.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
